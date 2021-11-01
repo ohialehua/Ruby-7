0c83ca5959a7441042f7084538e4386a22a68652
EC2 Default User <ec2-user@ip-172-31-79-219.ec2.internal>
2021-11-01 19:30:10 +0000

    add

--- lesson7-2.rb
+++ lesson7-2.rb
@@ -4,8 +4,13 @@ n = gets.to_i
 for i in 1..n do
   puts "#{i}回目の計算
   2つの値を入力してください"
+
   a = gets.to_i
   b = gets.to_i
+
+puts "a=#{a}"
+puts "b=#{b}"
+
 puts "計算結果を出力します"
 puts "a+b=#{a+b}"
 puts "a-b=#{a-b}"
