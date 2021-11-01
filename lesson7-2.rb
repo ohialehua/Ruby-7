eab3ec027e2566341d208fa110e436ecde5dd609
EC2 Default User <ec2-user@ip-172-31-79-219.ec2.internal>
2021-11-01 19:23:39 +0000

    Inical Commit

---
+++ lesson7-2.rb
@@ -0,0 +1,15 @@
+puts "計算をはじめます
+何回計算を繰り返しますか？"
+n = gets.to_i
+for i in 1..n do
+  puts "#{i}回目の計算
+  2つの値を入力してください"
+  a = gets.to_i
+  b = gets.to_i
+puts "計算結果を出力します"
+puts "a+b=#{a+b}"
+puts "a-b=#{a-b}"
+puts "a*b=#{a*b}"
+puts "a/b=#{a/b}"
+puts "計算を終了します"
+end
